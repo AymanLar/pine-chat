d3e475db8a1adc2739cd5ffc4adb4fbabb4b5e1b
AymanLar <larourayman@gmail.com>
2022-05-18 20:26:12 +0100

    setup the server :100:
    - create the backend folder .
    - install dependencies .
    - setup express server .
    - connect socket.io with the express server.

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    // when a user joins the chat, we emit a message to the client //
+    console.log("user Connected", socket.id);
+
+    // when a user leaves the chat, we emit a message to the client //
+    socket.on('disconnect', () => {
+        console.log("user Disconnected", socket.id);
+    });
+});
+
+
+server.listen(3001, () => {
+    console.log('The Server is running on port 3001 ...');
+});
